cb538cc1ca42661606abbe653ca03609e1c0073a
julie-dng <juliedienger17@gmail.com>
2021-09-07 10:48:19 +0200

    Remise à Zero

--- Template/header.php
+++ Template/header.php
@@ -12,92 +12,31 @@
    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
   <link rel="stylesheet" href="assets/style-header.css">
 </head>
-
 <body>
-
-<div class="page-wrapper chiller-theme toggled">
-  <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
-    <i class="fas fa-bars"></i>
-  </a>
-  <nav id="sidebar" class="sidebar-wrapper">
-    <div class="sidebar-content">
-      <div class="sidebar-brand">
-        <img src="assets/images/rec.png" alt="Logo Rec" height="50">
-      </div>
-      <div class="sidebar-header">
-        <div class="user-pic">
-          <img class="img-responsive img-rounded" src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg"
-            alt="User picture">
-        </div>
-        <div class="user-info">
-          <span class="user-name">Quentin
-            <strong>Felbinger</strong>
-          </span>
-          <span class="user-role">Administrator</span>
-
-        </div>
-      </div>
-      <div class="sidebar-menu">
-        <ul>
-          <li class="header-menu">
-            <span>General</span>
-          </li>
-          <li class="sidebar">
-            <a href="#">
-              <i class="fa fa-tachometer-alt"></i>
-              <span>Dashboard</span>
-
-            </a>
-
-          </li>
-          <li class="sidebar">
-            <a href="#">
-              <i class="fa fa-shopping-cart"></i>
-              <span>E-commerce</span>
-
-            </a>
-
-          </li>
-          <li class="sidebar">
-            <a href="#">
-              <i class="far fa-gem"></i>
-              <span>Components</span>
-            </a>
-            <div class="sidebar-submenu">
-
-            </div>
-          </li>
-          <li class="sidebar">
-            <a href="#">
-              <i class="fa fa-chart-line"></i>
-              <span>Charts</span>
-            </a>
-
-          </li>
-          <li class="sidebar">
-            <a href="#">
-              <i class="fa fa-globe"></i>
-              <span>Maps</span>
-            </a>
-
-          </li>
-
-        </ul>
-      </div>
-      <!-- sidebar-menu  -->
+  <header id="sidebar">
+    <div class="text-center">
+      <a href="#">
+        <img src="assets/images/rec.png" alt="" height="70">
+      </a>
+      <hr/>
     </div>
-    <!-- sidebar-content  -->
+    <div class="mt-5 d-flex p-2">
+      <img src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg" alt="profil" height="70">
+      <div class="d-flex flex-column p-2">
+      <p>Quentin Felbinger
+      <span>Administrateur</span>
+      </p>
+    </div>
+    </div>
+      <nav class="mt-5">
+            <a href="#" class="m-2"> <i class="fa fa-sign-in-alt"></i> BLABLABL</a>
 
-  </nav>
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-    crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-    crossorigin="anonymous"></script>
-<script type="text/javascript" src="assets/app.js">
+            <a href="#" class="m-2"><i class="fa fa-sign-in-alt"></i> BLABLABL</a>
 
-</script>
+            <a href="#" class="m-2"> <i class="fa fa-sign-in-alt"></i> BLABLABL</a>
+
+      </nav>
+      <hr/>
+  </header>
 </body>
-
 </html>
