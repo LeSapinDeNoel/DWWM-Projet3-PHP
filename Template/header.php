58bb1b117e351e02b073d586e1ae4e0108107801
Quentin <quentin.pisli@gmail.com>
2021-09-07 11:34:28 +0200

    navigation première étape

--- Template/header.php
+++ Template/header.php
@@ -5,45 +5,61 @@
   <meta charset="utf-8">
   <title>Accueil</title>
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
        crossorigin="anonymous">
    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
   <link rel="stylesheet" href="assets/style-header.css">
 </head>
 <body>
   <header id="sidebar">
+
     <div class="text-center">
+
       <a href="#" class="mb-2">
         <img src="assets/images/rec.png" alt="" height="70">
       </a>
+
       <hr/>
+
     </div>
+
     <div class="d-flex p-2 profil">
+
       <img src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg" alt="profil" height="70">
+      
       <div class="d-flex flex-column p-2">
-      <p><a href="#">Quentin Felbinger</a>
-      <span>Administrateur</span>
-      </p>
-    </div>
+
+        <p><a href="#">Quentin Felbinger</a>
+          <span>Administrateur</span>
+        </p>
+
+      </div>
 
     </div>
-    <div class="">
-      <hr/>
-    </div>
-      <nav class="mt-5">
-            <a href="#" class="m-2"> <i class="fa fa-sign-in-alt"></i> Les critiques</a>
 
-            <hr/>
+    <hr/>
 
-            <a href="#" class="m-2"><i class="fa fa-sign-in-alt"></i> BLABLABL</a>
+  <nav class="mt-5">
 
-            <hr/>
+    <a href="#"> <i class="far fa-newspaper"></i> Les critiques</a>
 
-            <a href="#" class="m-2"> <i class="fa fa-sign-in-alt"></i> BLABLABL</a>
+    <a href="#"> <i class="fas fa-user"></i> Se connecter</a>
 
-      </nav>
+    <a href="#"> <i class="fas fa-sign-in-alt"></i> S'inscrire</a>
+
+    <a href="#"> <i class="far fa-sticky-note"></i> Mes critiques</a>
+
+    <a href="#"> <i class="fas fa-toggle-on"></i> Modération</a>
+
+    <a href="#"> <i class="fas fa-users"></i> Les utilisateurs</a>
+
+    <a href="#"> <i class="fas fa-sign-out-alt"></i> Se déconnecter</a>
+
+    <a href="#"> <i class="fas fa-users"></i> Nous contacter</a>
+
+    </nav>
 
   </header>
 </body>
