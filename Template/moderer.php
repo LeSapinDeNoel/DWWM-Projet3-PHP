bd0029b3ea6baca5c48bbaa889a69753aaa04301
julie-dng <juliedienger17@gmail.com>
2021-09-08 15:00:14 +0200

    page pour moderateur
    parge avec la listes des critiques pour le moderateur

---
+++ Template/moderer.php
@@ -0,0 +1,117 @@
+
+<?php
+
+    // J'affiche le header
+include("header.php");
+
+?>
+<main>
+  <div class="justify-content-center">
+  <h1>Listes des critiques</h1>
+  <hr>
+  </div>
+
+  <table class="table table-reflow">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Titre</th>
+        <th>Auteur</th>
+        <th>Visibilité</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="tr_height">
+        <th scope="row">1</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>
+          <div class="form-check form-switch ml-4">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+          </div>
+        </td>
+      </tr>
+      <tr class="tr_height">
+        <th scope="row">2</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>
+          <div class="form-check form-switch ml-4">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+          </div>
+        </td>
+      </tr>
+      <tr class="tr_height">
+        <th scope="row">3</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>
+          <div class="form-check form-switch ml-4">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+          </div>
+        </td>
+      </tr>
+
+      <tr class="tr_height">
+        <th scope="row">4</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>
+          <div class="form-check form-switch ml-4">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+          </div>
+        </td>
+      </tr>
+
+      <tr class="tr_height">
+        <th scope="row">5</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>
+          <div class="form-check form-switch ml-4">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+          </div>
+        </td>
+      </tr>
+
+      <tr class="tr_height">
+        <th scope="row">6</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>
+          <div class="form-check form-switch ml-4">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+          </div>
+        </td>
+      </tr>
+
+      <tr class="tr_height">
+        <th scope="row">7</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>
+          <div class="form-check form-switch ml-4">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+          </div>
+        </td>
+      </tr>
+
+      <tr class="tr_height">
+        <th scope="row">8</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>
+          <div class="form-check form-switch ml-4">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked
+          </div>
+        </td>
+      </tr>
+
+
+    </tbody>
+  </table>
+</main>
+
+<?php
+    // j'affiche le footer
+// include("footer.php");
