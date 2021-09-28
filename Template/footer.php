00a09a5d1cdeed510d401ce7ced793410a96937a
Quentin <quentin.pisli@gmail.com>
2021-09-28 11:21:35 +0200

    Footer update

--- Template/footer.php
+++ Template/footer.php
@@ -1,5 +1,11 @@
 <footer id="footer">
-    @copyright 2021 REC
+    <nav>
+        <a href="#">Accueil</a>
+        <a href="#">Contact</a>
+        <a href="#">Aide</a>
+        <a href="#">Mentions légales</a>
+    </nav>
+    <span>@copyright 2021 REC</span>
 </footer>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript" src="assets/app.js">
