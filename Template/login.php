1fe734c6ca519066380f6b67229b9cca155e735b
julie-dng <juliedienger17@gmail.com>
2021-09-28 09:50:47 +0200

    Création de la page login

---
+++ Template/login.php
@@ -0,0 +1,16 @@
+<?php
+  // J'affiche le header
+  include("header.php");
+?>
+
+<main>
+  <div class="justify-content-center">
+    <h1>Se connecter</h1>
+    <hr>
+  </div>
+
+</main>
+<?php
+  // j'affiche le footer
+ include("footer.php");
+?>
