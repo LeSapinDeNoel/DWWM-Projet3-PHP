a242a1bf8725151182bcdddc325e127f29727f47
julie-dng <juliedienger17@gmail.com>
2021-09-28 10:51:11 +0200

    formulaire de recherche de la page critic

--- Template/critic.php
+++ Template/critic.php
@@ -7,54 +7,80 @@ include("header.php");
 
 <main>
 
-<h1>Les critiques</h1>
-<hr>
+  <h1>Les critiques</h1>
+  <hr>
 
-<form action="#" method="post">
+  <form action="#" method="post">
+    <div id="formulaire_search">
 
-</form>
+      <div class="form-group a">
+        <label for="keyword">Mot clé</label>
+        <input id="keyword" type="text">
+      </div>
+      <div class="form-group a">
+        <label for="keyword">Catégorie</label>
+        <input id="categorie" type="text">
+      </div>
+      <div class="form-group a">
+        <label for="createur">Créateur</label>
+        <input id="createur" type="text">
+      </div>
+      <div class="form-group a">
+        <label for="date">Date exact</label>
+        <input type="date" id="start" name="trip-start" value="">
+      </div>
+      <div class="form-group a">
+        <label for="startdate">Date de début</label>
+        <input id="startdate" type="date" name="startdate" />
+      </div>
+      <div class="form-group a">
+        <label for="enddate">Date de fin</label>
+        <input id="enddate" type="date" name="enddate" />
+      </div>
+    </div>
+  </form>
 
-<section id="galerie">
+  <section id="galerie">
 
-  <div>
+    <div>
 
-    <article>
-      <img src="https://fakeimg.pl/320x220/?text=oui">
-      <h2>Article 1</h2>
-    </article>
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
 
-    <article>
-      <img src="https://fakeimg.pl/320x220/?text=oui">
-      <h2>Article 1</h2>
-    </article>
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
 
-    <article>
-      <img src="https://fakeimg.pl/320x220/?text=oui">
-      <h2>Article 1</h2>
-    </article>
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
 
-    <article>
-      <img src="https://fakeimg.pl/320x220/?text=oui">
-      <h2>Article 1</h2>
-    </article>
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
 
-    <article>
-      <img src="https://fakeimg.pl/320x220/?text=oui">
-      <h2>Article 1</h2>
-    </article>
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
 
-    <article>
-      <img src="https://fakeimg.pl/320x220/?text=oui">
-      <h2>Article 1</h2>
-    </article>
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
 
-  </div>
+    </div>
 
-</section>
+  </section>
 
-<div class="PositionVbutton">
+  <div class="PositionVbutton">
     <button id="Vbutton">VOIR PLUS</button>
-</div>
+  </div>
 
 </main>
 
--- Template/profile.php
+++ Template/profile.php
@@ -9,65 +9,65 @@
     <hr>
   </div>
 
-<div class="conteneur_profil">
+  <div class="conteneur_profil">
 
-  <form action="#" method="post" enctype="multipart/form-data">
+    <form action="#" method="post" enctype="multipart/form-data">
 
-<div id="image_profil">
-
-
-  <label for="fileToUpload">
-    <div class="profile-pic" style="background-image: url('https://fakeimg.pl/150x150/')">
-       <span></span>
-       <span>Modifier votre avatar</span>
-   </div>
- </label>
- <input type="File" name="fileToUpload" id="fileToUpload">
- </div>
-    	<div class="grid">
-    		<div class="form-group a">
-    			<label for="name">Nom</label>
-    			<input id="name" type="text">
-    		</div>
-
-    		<div class="form-group b">
-    			<label for="first-name">Prénom</label>
-    			<input id="first-name" type="text">
-    		</div>
-
-    		<div class="form-group email-group">
-    			<label for="email">Email</label>
-    			<input id="email" type="text">
-    		</div>
-
-    		<div class="form-group">
-    			<label for="role">Role</label>
-    			<input id="role" type="text">
-    		</div>
-
-    		<div class="form-group">
-    			<label for="pwd">Mots de passe</label>
-    			<input id="pwd" type="text">
-    		</div>
-
-    		<div class="form-group">
-    			<label for="confirm_pwd">Confirmation du mots de passe</label>
-    			<input id="confirm_pwd" type="text">
-    		</div>
-    	</div>
+      <div id="image_profil">
 
 
-    	<div class="button-container">
-    		<button class="button">Enregister les modifications</button>
-    	</div>
-  </form>
+        <label for="fileToUpload">
+          <div class="profile-pic" style="background-image: url('https://fakeimg.pl/150x150/')">
+            <span></span>
+            <span>Modifier votre avatar</span>
+          </div>
+        </label>
+        <input type="File" name="fileToUpload" id="fileToUpload">
+      </div>
+      <div class="grid">
+        <div class="form-group a">
+          <label for="name">Nom</label>
+          <input id="name" type="text">
+        </div>
 
-</div>
+        <div class="form-group b">
+          <label for="first-name">Prénom</label>
+          <input id="first-name" type="text">
+        </div>
 
-<style>
+        <div class="form-group email-group">
+          <label for="email">Email</label>
+          <input id="email" type="text">
+        </div>
+
+        <div class="form-group">
+          <label for="role">Role</label>
+          <input id="role" type="text">
+        </div>
+
+        <div class="form-group">
+          <label for="pwd">Mots de passe</label>
+          <input id="pwd" type="text">
+        </div>
+
+        <div class="form-group">
+          <label for="confirm_pwd">Confirmation du mots de passe</label>
+          <input id="confirm_pwd" type="text">
+        </div>
+      </div>
 
 
-</style>
+      <div class="button-container">
+        <button class="button">Enregister les modifications</button>
+      </div>
+    </form>
+
+  </div>
+
+  <style>
+
+
+  </style>
 
 </main>
 <?php
