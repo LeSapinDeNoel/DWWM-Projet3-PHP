4184b1a0823794e6d3dbe29769204940af5b639e
Quentin <quentin.pisli@gmail.com>
2021-09-28 13:59:53 +0200

    Create user_critic.php

---
+++ Template/user_critic.php
@@ -0,0 +1,84 @@
+<?php
+
+    // J'affiche le header
+include("header.php");
+
+?>
+
+<main>
+
+  <h1>Mes critiques</h1>
+  <hr>
+
+  <form action="#" method="post">
+    <div id="formulaire_search">
+
+      <div class="form-group a">
+        <label for="keyword">Mot clé</label>
+        <input id="keyword" type="text">
+      </div>
+      <div class="form-group a">
+        <label for="date">Date exact</label>
+        <input type="date" id="start" name="trip-start" value="">
+      </div>
+      <div class="form-group a">
+        <label for="startdate">Date de début</label>
+        <input id="startdate" type="date" name="startdate" />
+      </div>
+      <div class="form-group a">
+        <label for="enddate">Date de fin</label>
+        <input id="enddate" type="date" name="enddate" />
+      </div>
+    </div>
+  </form>
+
+  <section id="galerie">
+
+    <div>
+
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
+
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
+
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
+
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
+
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
+
+      <article>
+        <img src="https://fakeimg.pl/320x220/?text=oui">
+        <h2>Article 1</h2>
+      </article>
+
+    </div>
+
+  </section>
+
+  <div class="PositionVbutton">
+    <button id="Vbutton">VOIR PLUS</button>
+  </div>
+
+</main>
+
+<?php
+
+// j'affiche le footer
+include("footer.php");
+
+?>
