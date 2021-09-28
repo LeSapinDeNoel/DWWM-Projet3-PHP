b06ee20fefbdbbfbfc11cea34371be62dbe4b65f
julie-dng <juliedienger17@gmail.com>
2021-09-28 09:47:58 +0200

    image profil modifiable
    Modifier l'image de profil dans le formulaire de la page profil

--- Template/profile.php
+++ Template/profile.php
@@ -11,46 +11,109 @@
 
 <div class="conteneur_profil">
 
+  <form action="#" method="post" enctype="multipart/form-data">
 
-	<div class="grid">
-		<div class="form-group a">
-			<label for="name">Nom</label>
-			<input id="name" type="text">
-		</div>
-
-		<div class="form-group b">
-			<label for="first-name">Prénom</label>
-			<input id="first-name" type="text">
-		</div>
-
-		<div class="form-group email-group">
-			<label for="email">Email</label>
-			<input id="email" type="text">
-		</div>
-
-		<div class="form-group">
-			<label for="role">Role</label>
-			<input id="role" type="text">
-		</div>
-
-		<div class="form-group">
-			<label for="pwd">Mots de passe</label>
-			<input id="pwd" type="text">
-		</div>
-
-		<div class="form-group">
-			<label for="confirm_pwd">Confirmation du mots de passe</label>
-			<input id="confirm_pwd" type="text">
-		</div>
-	</div>
+<div id="image_profil">
 
 
-	<div class="button-container">
-		<button class="button">Enregister les modifications</button>
-	</div>
+  <label for="fileToUpload">
+    <div class="profile-pic" style="background-image: url('https://fakeimg.pl/150x150/')">
+       <span></span>
+       <span>Modifier votre avatar</span>
+   </div>
+ </label>
+ <input type="File" name="fileToUpload" id="fileToUpload">
+ </div>
+    	<div class="grid">
+    		<div class="form-group a">
+    			<label for="name">Nom</label>
+    			<input id="name" type="text">
+    		</div>
+
+    		<div class="form-group b">
+    			<label for="first-name">Prénom</label>
+    			<input id="first-name" type="text">
+    		</div>
+
+    		<div class="form-group email-group">
+    			<label for="email">Email</label>
+    			<input id="email" type="text">
+    		</div>
+
+    		<div class="form-group">
+    			<label for="role">Role</label>
+    			<input id="role" type="text">
+    		</div>
+
+    		<div class="form-group">
+    			<label for="pwd">Mots de passe</label>
+    			<input id="pwd" type="text">
+    		</div>
+
+    		<div class="form-group">
+    			<label for="confirm_pwd">Confirmation du mots de passe</label>
+    			<input id="confirm_pwd" type="text">
+    		</div>
+    	</div>
+
+
+    	<div class="button-container">
+    		<button class="button">Enregister les modifications</button>
+    	</div>
+  </form>
 
 </div>
+
 <style>
+
+
+/* IMAGES AVATAR  */
+#image_profil{
+  text-align: center;
+}
+/* mises en form de l'images */
+.profile-pic {
+    border-radius: 50%;
+    height: 150px;
+    width: 150px;
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: multiply;
+    vertical-align: middle;
+    text-align: center;
+    color: transparent;
+    transition: all .3s ease;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+/* images qui devient sombre au hover */
+.profile-pic:hover {
+    background-color: rgba(0,0,0,.5);
+    z-index: 10000;
+    color: #fff;
+    transition: all .3s ease;
+    text-decoration: none;
+}
+
+
+/* texte changement d'images */
+.profile-pic span {
+    display: inline-block;
+    padding-bottom: 4.5em;
+}
+
+/* Pour faire disparaitre le "choisir un fichier" */
+form input[type="file"] {
+        display: none;
+        cursor: pointer;
+ }
+
+
+/* FIN IMAGES AVATAR */
+
+
+/* Style du formulaire */
 
 .conteneur_profil {
   width: 80%;
@@ -97,7 +160,7 @@
 @media screen and (min-width: 1024px) {
 
   .conteneur_profil {
-      margin-top: 100px;
+      margin-top: 50px;
   }
 
 
@@ -118,7 +181,7 @@
 		width: auto;
 	}
 }
-
+/* Fin Style du formulaire */
 </style>
 
 </main>
