06acde63f835993debfa3ae063a899217bae300a
Quentin <quentin.pisli@gmail.com>
2021-09-28 10:21:39 +0200

    Page critic maj
    - Ajout de l'emplacement du formulaire
    - Les cards articles opérationnelles
    - Le bouton voir plus avec sont style

--- Template/critic.php
+++ Template/critic.php
@@ -7,6 +7,54 @@ include("header.php");
 
 <main>
 
+<h1>Les critiques</h1>
+<hr>
+
+<form action="#" method="post">
+
+</form>
+
+<section id="galerie">
+
+  <div>
+
+    <article>
+      <img src="https://fakeimg.pl/320x220/?text=oui">
+      <h2>Article 1</h2>
+    </article>
+
+    <article>
+      <img src="https://fakeimg.pl/320x220/?text=oui">
+      <h2>Article 1</h2>
+    </article>
+
+    <article>
+      <img src="https://fakeimg.pl/320x220/?text=oui">
+      <h2>Article 1</h2>
+    </article>
+
+    <article>
+      <img src="https://fakeimg.pl/320x220/?text=oui">
+      <h2>Article 1</h2>
+    </article>
+
+    <article>
+      <img src="https://fakeimg.pl/320x220/?text=oui">
+      <h2>Article 1</h2>
+    </article>
+
+    <article>
+      <img src="https://fakeimg.pl/320x220/?text=oui">
+      <h2>Article 1</h2>
+    </article>
+
+  </div>
+
+</section>
+
+<div class="PositionVbutton">
+    <button id="Vbutton">VOIR PLUS</button>
+</div>
 
 </main>
 
