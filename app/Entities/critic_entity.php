ce71e9859007d1b04202486bbe6a77ef29bedf5f
julie-dng <76993533+julie-dng@users.noreply.github.com>
2021-11-05 14:16:58 +0100

    initialisation critic_entity et critic_model
    Co-Authored-By: Q_Sapheko <73527645+sapheko@users.noreply.github.com>

---
+++ app/Entities/critic_entity.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Entity;
+
+class Critic_entity extends Entity
+{
+
+}
---
+++ app/Models/critic_model.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class Critic_model extends Model
+{
+    protected $table         = 'critics';
+    protected $primaryKey    = 'critic_id';
+    protected $allowedFields = ['critic_creator', 'critic_title', 'critic_content', 'critic_createdate', 'critic_img', 'critic_status', 'critic_cat'];
+
+    protected $returnType    = 'App\Entities\critic_entity';
+    protected $useTimestamps = true;
+
+
+    // protected $createdField = 'cust_createdate';
+    // protected $updatedField = 'cust_updatedate';
+    // protected $deletedField = 'cust_deletedate';
+}
