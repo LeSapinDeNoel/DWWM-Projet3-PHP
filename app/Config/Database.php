07d547e4222cd361fd414e14c8614a134396df1c
julie-dng <juliedienger17@gmail.com>
2021-11-11 14:33:33 +0100

    initialisation travail maison
    Pour le fonctionnement de localhost:8888 sur mamp

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
         'DSN'      => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => '',
+        'password' => 'root',
         'database' => 'rec_bdd',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
