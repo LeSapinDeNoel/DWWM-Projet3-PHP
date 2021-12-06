717f68a086a952e022692087e8f1992bd690c20c
QuentinMSI <quentin.pisli@gmail.com>
2021-12-06 12:01:00 +0100

    Fix user model

--- app/Models/user_model.php
+++ app/Models/user_model.php
@@ -10,7 +10,6 @@ class User_model extends Model
     protected $allowedFields = ['user_name', 'user_firstname', 'user_mail', 'user_pwd', 'user_avatar', 'user_role'];
 
     protected $returnType    = 'App\Entities\user_entity';
-    protected $useTimestamps = true;
 
     public function findAllWithCat(){
         $this->join('category', 'cat_id = critic_cat');
