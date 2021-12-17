c4a3e98ab9d86053778a62e92d61684f93c52bd9
QuentinMSI <quentin.pisli@gmail.com>
2021-12-17 14:41:04 +0100

    Fonction check pwd login
    Vérifie le mdp pour la connexion mais seulement les mdp en clair et non haché

--- app/Libraries/Hash.php
+++ app/Libraries/Hash.php
@@ -8,6 +8,21 @@ class Hash
     {
         return password_hash($password, PASSWORD_BCRYPT);
     }
+
+    public static function check($form_pwd, $db_pwd)
+    {
+        // if(password_verify($form_pwd, $db_pwd)) {
+        //     return true;
+        // }else {
+        //     return false;
+        // }
+
+        if($form_pwd == $db_pwd) {
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
 
 ?>
